8325523ac211d52a3478828d4724cbf5bdc07d0a
Jainam2204 <jainamvora2201@gmail.com>
2025-12-08 09:53:47 +0530

    Chat bug fixed

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const getChatUserList  = require('../controllers/chatController');
 const auth = require('../middleware/authMiddleware');
 const checkBanned = require("../middleware/checkBanned");
+
 router.get('/connections', auth,checkBanned, getChatUserList);
 
 module.exports = router;
